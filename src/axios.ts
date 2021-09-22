3f1894ccccb4c72d411952e1a6f5b0de57799de7
vill <villdevelop@gmail.com>
2021-09-22 16:31:46 +0530

    fix: updated backend url

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://vidi-backend.herokuapp.com/",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://vidi-backend.herokuapp.com/",
   // withCredentials: true,
 });
